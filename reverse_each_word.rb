53491469394b6dafb25d456370e6301834372106
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-28 16:40:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 
-def reverse_each_word(string)
-  words = string.split(" ")
-  words.each {|word| 
-    word.reverse
-  }
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
   reversed.join(" ")
 end
-
- 
