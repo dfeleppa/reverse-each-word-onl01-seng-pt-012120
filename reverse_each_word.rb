0996f76d06c12c1a79c9d0b3729a2d6dd4a4010b
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-28 16:42:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect {|word|
-  word.reverse
+  reversed = sentence.split(" ").collect {|matt|
+  matt.reverse
   }
   reversed.join(" ")
 end
